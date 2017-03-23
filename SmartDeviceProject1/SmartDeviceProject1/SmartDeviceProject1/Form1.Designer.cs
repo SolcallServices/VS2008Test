29825703ae1df642efc734ecfdb31942921ce3d8
guptapushpak1 <guptapushpak@yahoo.co.in>
2017-03-23 18:25:42 +1100

    Button size change

--- SmartDeviceProject1/SmartDeviceProject1/SmartDeviceProject1/Form1.Designer.cs
+++ SmartDeviceProject1/SmartDeviceProject1/SmartDeviceProject1/Form1.Designer.cs
@@ -40,7 +40,7 @@
             // 
             this.button1.Location = new System.Drawing.Point(48, 26);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(203, 75);
+            this.button1.Size = new System.Drawing.Size(149, 21);
             this.button1.TabIndex = 0;
             this.button1.Text = "Jaswinder1st";
             this.button1.Click += new System.EventHandler(this.button1_Click);
