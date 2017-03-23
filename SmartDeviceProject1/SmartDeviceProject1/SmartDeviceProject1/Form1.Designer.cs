d4eb52d9edfc50add0c541981e2ecfd181a9d41b
guptapushpak1 <guptapushpak@yahoo.co.in>
2017-03-23 14:48:43 +1100

    Test Pushpak

--- SmartDeviceProject1/SmartDeviceProject1/SmartDeviceProject1/Form1.Designer.cs
+++ SmartDeviceProject1/SmartDeviceProject1/SmartDeviceProject1/Form1.Designer.cs
@@ -31,6 +31,7 @@
         {
             this.mainMenu1 = new System.Windows.Forms.MainMenu();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -41,12 +42,22 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "Jaswinder";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(103, 276);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(72, 20);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Pushpak";
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.AutoScroll = true;
             this.ClientSize = new System.Drawing.Size(328, 455);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Menu = this.mainMenu1;
             this.Name = "Form1";
@@ -58,6 +69,7 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
