354ebe29c2cdda9504d180b8def4d354ce2ff35e
Jaswinder Singh <jaswinder.singh@solcall.com.au>
2017-03-23 21:18:26 +1100

    Button Renaming Changes

--- SmartDeviceProject1/SmartDeviceProject1/SmartDeviceProject1/Form1.Designer.cs
+++ SmartDeviceProject1/SmartDeviceProject1/SmartDeviceProject1/Form1.Designer.cs
@@ -45,7 +45,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(203, 75);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Jaswinder1st";
+            this.button1.Text = "Jaswinder";
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
@@ -54,7 +54,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(203, 65);
             this.button2.TabIndex = 1;
-            this.button2.Text = "Pushpak 1st";
+            this.button2.Text = "Pushpak";
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
@@ -63,7 +63,7 @@
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(203, 65);
             this.button3.TabIndex = 2;
-            this.button3.Text = "Kutta123123";
+            this.button3.Text = "Testing";
             // 
             // button4
             // 
